cdd551d9cbc6bd9cb5da49a586326e455f79b0c7
raulcsj <raulcsj@126.com>
2014-08-20 14:39:51 +0800

    improve

--- util/src/main/java/common/util/ViewConstants.java
+++ util/src/main/java/common/util/ViewConstants.java
@@ -1,16 +1,25 @@
-/*
- * The MIT License (MIT)
- * Copyright (c) 2013 newgxu.cn <the original author or authors>.
- * The software shall be used for good, not evil.
- */
+/************************************************************************
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+
+     http://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+ ************************************************************************/
 package common.util;
 
 /**
- * 一些视图常量，一般都是lab中约定的。
- *
- * @User longkai
- * @Date 13-3-29
- * @Mail im.longkai@gmail.com
+ * 一些视图常量。
+ * 
+ * @author CSJ
+ * @email raulcsj@126.com
+ * @create 2014年8月20日
+ * @version 1.0
  */
 public class ViewConstants {
 
